f873948ba0f9018812ab0b811f79f3b12f763657
Matheus Felipe <matheusibnf@hotmail.com>
2024-06-26 17:12:02 -0300

    git first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const screen1 = document.querySelector("screen1");
+const screen2 = document.querySelector("screen2");
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleTryClick);
+document.addEventListener("keydown", function (e) {
+  if (e.key == "Enter" && screen1.classList.contains("hide")) {
+    handleResetClick();
+  }
+});
+
+//funções callback
+function handleTryClick(event) {
+  event.preventDefault();
+
+  const inputNumber = document.querySelector("#inputNumber");
+
+  if (Number(inputNumber.value) == randomNumber) {
+    toggleScreen();
+
+    screen2.querySelector(
+      "h2"
+    ).innerText = `Acertou em ${xAttempts} tentativas`;
+  }
+  inputNumber.value = "";
+  xAttempts++;
+}
+
+function handleResetClick() {
+  toggleScreen();
+  xAttempts = 1;
+  randomNumber = Math.round(Math.random() * 10);
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+}
