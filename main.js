d8514b9ab6836ff5f66252d3365723ac3d4c3c28
Matheus Felipe <matheusibnf@hotmail.com>
2024-06-27 18:10:02 -0300

    git putting functions

--- main.js
+++ main.js
@@ -1,40 +1,48 @@
-const screen1 = document.querySelector("screen1");
-const screen2 = document.querySelector("screen2");
-const btnTry = document.querySelector("#btnTry");
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const boxbiscoito = document.querySelector("#boxbiscoito");
 const btnReset = document.querySelector("#btnReset");
 
-btnTry.addEventListener("click", handleTryClick);
-btnReset.addEventListener("click", handleTryClick);
+const fortune = [
+  
+  "Você tem os olhos e o olhar mais encantador, e cativante que já vi nessa vida!",
+  "Você jamais estará sozinha nas dificuldades, pois o seu noivo, nunca a deixará!",
+  "Você tem uma beleza incompáravel a qualquer ser nesse universo!!",
+  "Dentre essas duas famílias, existe alguém que mataria e morreria por você!",
+  "Você tem 2 famílias, que a ama demais, e que cuida e se importa com você!",
+  "A sua sorte é ter um noivo como o Matheus, para cuidar e amar você pra sempre!",
+  "Por mais que não acredite em sorte, e sim em Deus, você é a pessoa mais sortuda e abençoada dessa vida!"
+  
+  
+  
+];
+
+boxbiscoito.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
 document.addEventListener("keydown", function (e) {
-  if (e.key == "Enter" && screen1.classList.contains("hide")) {
+  if (e.key == "Enter" && screen2.classList.contains("hide")) {
+    handleTryClick();
+  } else if (e.key == "Enter" && screen1.classList.contains("hide")) {
     handleResetClick();
   }
 });
 
-//funções callback
 function handleTryClick(event) {
-  event.preventDefault();
-
-  const inputNumber = document.querySelector("#inputNumber");
-
-  if (Number(inputNumber.value) == randomNumber) {
-    toggleScreen();
-
-    screen2.querySelector(
-      "h2"
-    ).innerText = `Acertou em ${xAttempts} tentativas`;
-  }
-  inputNumber.value = "";
-  xAttempts++;
+  toggleScreen();
+  pickFortune();
 }
 
 function handleResetClick() {
   toggleScreen();
-  xAttempts = 1;
-  randomNumber = Math.round(Math.random() * 10);
+}
+
+function pickFortune() {
+  let allFortunes = fortune.length;
+  let randomNumber = Math.floor(Math.random() * allFortunes);
+  screen2.querySelector("#boxparagraph").innerText = `${fortune[randomNumber]}`;
 }
 
 function toggleScreen() {
-  screen1.classList.toggle("hide");
   screen2.classList.toggle("hide");
+  screen1.classList.toggle("hide");
 }
