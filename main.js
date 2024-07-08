b1c4b48d046f759dc381d523cd8fb4ad0824343d
Matheus Felipe <matheusibnf@hotmail.com>
2024-07-08 14:48:15 -0300

    changing the phrases

--- main.js
+++ main.js
@@ -5,13 +5,17 @@ const btnReset = document.querySelector("#btnReset");
 
 const fortune = [
   
-  "Você tem os olhos e o olhar mais encantador, e cativante que já vi nessa vida!",
-  "Você jamais estará sozinha nas dificuldades, pois o seu noivo, nunca a deixará!",
-  "Você tem uma beleza incompáravel a qualquer ser nesse universo!!",
-  "Dentre essas duas famílias, existe alguém que mataria e morreria por você!",
-  "Você tem 2 famílias, que a ama demais, e que cuida e se importa com você!",
-  "A sua sorte é ter um noivo como o Matheus, para cuidar e amar você pra sempre!",
-  "Por mais que não acredite em sorte, e sim em Deus, você é a pessoa mais sortuda e abençoada dessa vida!"
+  "O aprendizado é como o horizonte: não há limites.",
+  "Não há que ser forte, há que ser flexível.",
+  "Limitações são fronteiras criadas apenas pela nossa mente.",
+  "O cão não ladra por valentia e sim por medo.",
+  "Procure acender uma vela em vez de amaldiçoar a escuridão.",
+  "A palavra é prata, o silêncio é ouro.",
+  "Lembre-se de que grandes realizações e grandes amores envolvem grandes riscos.",
+  "Um pouco de perfume sempre fica nas mãos de quem oferece flores.",
+  "O homem só envelhece quando os lamentos substituem seus sonhos.",
+  "A persistência realiza o impossível.",
+  "Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu."
   
   
   
